597e0f0061499f645308857c3287604eefde76ad
madelynm3 <79669704+madelynm3@users.noreply.github.com>
2021-03-17 17:03:05 -0600

    update

--- speed_template/speed/game/word.py
+++ speed_template/speed/game/word.py
@@ -1,3 +1,6 @@
+from typing import Text
+
+
 class Word:
     def __init__(self):
         """
@@ -11,12 +14,18 @@ class Word:
         Displays word and moves it across the screen. Boolean which evaluates to
         True when on the screen and False when off
         """
+        self.move_word = self.text
+        return self.velocity
+
 
     def points(self):
         """
         Calculates points. If user typed word, points are the sum of the 
-        total letters in the word. If typed incorrectly, lose points calculated 
-        the same.
+        total letters in the word. If typed incorrectly, player receives 0 points.
         """
+        if word == input:
+            points = sum(len(word))
+        else:
+            points = 0 
     
 
